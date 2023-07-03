642b5c3d87ef1faf11674a0d47f0e64491b4a22f
harisvengattiri <69184472+harisvengattiri@users.noreply.github.com>
2023-07-04 02:25:59 +0530

    Update Admin.php

--- src/Admin.php
+++ src/Admin.php
@@ -2,14 +2,33 @@
 
 namespace Mancon;
 
+use Mancon\Exceptions\SystemErrorException;
+use Mancon\Exceptions\AlreadyExistsException;
+use Mancon\Exceptions\StringTypeMismatchException;
+use Mancon\Exceptions\IntTypeMismatchException;
+
 class Admin implements AdminInterface {
+
+    private $repository;
+    private $Customer;
+
+    public function __construct($repository) {
+        $this->repository = $repository;
+    }
 
     public function listCustomers(): array {
         return [];
     }
 
     public function addCustomer(Customer $customer): void {
-
+        try {
+            $this->checkType($customer);
+            $this->repository->addCustomer($customer);
+        } catch (SystemErrorException $e) {
+            throw new SystemErrorException;
+        } catch (AlreadyExistsException $e) {
+            throw new AlreadyExistsException;
+        }
     }
 
     public function updateCustomer(Customer $customer): void {
@@ -90,8 +109,25 @@ class Admin implements AdminInterface {
     public function updateLocationFare(LocationFare $fare): void {
 
     }
-    
+
     public function deleteLocationFare(int $id): void {
 
+    }
+
+    public function checkType($customer) {
+        $name = $customer->getName();
+        $phone = $customer->getPhone();
+        function checkName($name) {
+            if(!is_string($name)) {
+                throw new StringTypeMismatchException;
+            }
+        }
+        function checkPhone($phone) {
+            if(!is_int($phone)) {
+                throw new IntTypeMismatchException;
+            }
+        }
+        checkName($name);
+        checkPhone($phone);
     }
 }
