46a333ee80027750b96fc6a5ea4be28a7575cbfb
harisvengattiri <69184472+harisvengattiri@users.noreply.github.com>
2023-07-04 02:11:46 +0530

    refactor Customer Class

--- src/Customer.php
+++ src/Customer.php
@@ -4,4 +4,19 @@ namespace Mancon;
 
 class Customer {
 
+    private $name;
+    private $phone;
+
+    public function __construct(array $customer) {
+        $this->name = $customer('name');
+        $this->phone = $customer('phone');
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getPhone() {
+        return $this->phone;
+    }
 }
